885dc58ff08c6267bde91c830c201f8e51054eb4
Edu Mesquita <105309529+DuMesk@users.noreply.github.com>
2024-12-17 21:29:30 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 const SUPABASE_URL = 'https://qevbdpznltrtgdbpmnqx.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFldmJkcHpubHRydGdkYnBtbnF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI4MzQxMzUsImV4cCI6MjA0ODQxMDEzNX0.22z6Exu2rP1iSmKWVQx3oZf_a8Hr2QFagYb8JHzOVNg';
 
-//const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
